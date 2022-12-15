eb522edc046ab02cb49c3ec39497e15e75c17d55
Usiel-03A7 <shebastyanmail.com>
2022-12-15 10:43:44 -0600

    uso del hook useState y manejo del useEffect

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/welcome';
 
+/**
+ * 
+ * @returns etiquetas asignadas
+ * 
+ */
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hola perra</p>
-        <Welcome message="Acceso correcto a los props" name ="Usiel"/> 
+        <p>Hola name</p>
+        <Welcome message="Acceso correcto a los props" name={"54"}/>  
       </header>
     </div>
   );
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,17 +1,30 @@
 //props son las propiedades que se tienen en la
-import {useState} from 'react';
-export default function Welcome (props){
-    const [counter, setCounter] = useState(0);
-    const {message, name} = props;
-    return (
-        <div>
-            <h2>Hola {name}</h2>
-            <h3>contador de react con hooks</h3>
-            <h4>la suma del contador es: {counter}:</h4>
-            <button type="submit" onClick={()=> setCounter(counter+1)}>
-                Sumar contador
-            </button>
-            
-        </div>
-    )
+import { useState, useEffect } from 'react'; // useState ayuda a saber 
+//el estado del componente y poder modificarlo
+
+/**
+ * @param {{ message: string, name: number }} props 
+ * @param {[conter: number] setCounter: function} useState_hook
+ */
+
+export default function Welcome({ message, name }) {
+  let [counter, setCounter] = useState(0);
+  const [semaforo, setSemaforo] = useState(false);
+
+  const contar = ()=>{
+     setCounter(counter+1)
+     setSemaforo(!semaforo)
+  };
+
+  return (
+    <div>
+      <h2>Hola {name}</h2>
+      <h3>contador de react con hooks</h3> {/**/}
+      <h4>la suma del contador es: {counter}:</h4>
+      <p>El semaforo está en color {semaforo ? "verde" : "rojo"}</p>
+      <button type="submit" onClick={contar}>
+        Sumar contador
+      </button>
+    </div>
+  )
 }
